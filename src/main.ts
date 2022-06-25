bee4e8a1cc83366d344490203221bf0b33cd19c0
somoore <shane.omoore@fwi.com>
2022-06-25 13:00:39 +0100

    feat: add pipe sprites

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ const gameConfig: Phaser.Types.Core.GameConfig = {
     arcade: {
       debug: true,
       gravity: {
-        y: 400,
+        // y: 400,
       },
     },
   },
@@ -20,6 +20,8 @@ const gameConfig: Phaser.Types.Core.GameConfig = {
   },
 };
 
+let upperPipe: Phaser.Physics.Arcade.Sprite;
+let lowerPipe: Phaser.Physics.Arcade.Sprite;
 let bird: Phaser.Physics.Arcade.Sprite;
 const FLAP_VELOCITY = 250;
 const VELOCITY = 200;
@@ -31,13 +33,18 @@ const initialPos = {
 function preload() {
   this.load.image('sky', 'assets/sky.png');
   this.load.image('bird', 'assets/bird.png');
+  this.load.image('pipe', 'assets/pipe.png');
 }
 
 function create() {
   this.add.image(0, 0, 'sky').setOrigin(0);
 
+  upperPipe = this.physics.add.sprite(400, 100, 'pipe').setOrigin(0, 1);
+  lowerPipe = this.physics.add.sprite(400, upperPipe.y + 100, 'pipe').setOrigin(0, 0);
+
   bird = this.physics.add.sprite(initialPos.x, initialPos.y, 'bird').setOrigin(0);
   bird.body.velocity.x = VELOCITY;
+  bird.body.gravity.y = 400;
 
   this.input.keyboard.on('keyup-SPACE', flap);
 }
