c5bc77243363ba39466fc48cfe79525fb78fc235
somoore <shane.omoore@fwi.com>
2022-06-25 13:32:04 +0100

    feat: :sparkles: more pipes + velocity

--- src/main.ts
+++ src/main.ts
@@ -21,19 +21,18 @@ const gameConfig: Phaser.Types.Core.GameConfig = {
 };
 
 const FLAP_VELOCITY = 250;
-const VELOCITY = 200;
 const initialPos = {
   x: (gameConfig.width as number) / 10,
   y: (gameConfig.height as number) / 2,
 };
+const PIPES_TO_RENDER = 4;
 
 let upperPipe: Phaser.Physics.Arcade.Sprite;
 let lowerPipe: Phaser.Physics.Arcade.Sprite;
 let bird: Phaser.Physics.Arcade.Sprite;
+let pipeHorizontalDistance = 0;
 
 const PIPE_DISTANCE_RANGE = [150, 250];
-const pipeVerticalDistance = Phaser.Math.Between(PIPE_DISTANCE_RANGE[0], PIPE_DISTANCE_RANGE[1]);
-const pipeVerticalPosition = Phaser.Math.Between(0 + 20, (gameConfig.height as number) - 20 - pipeVerticalDistance);
 
 function preload() {
   this.load.image('sky', 'assets/sky.png');
@@ -44,11 +43,21 @@ function preload() {
 function create() {
   this.add.image(0, 0, 'sky').setOrigin(0);
 
-  upperPipe = this.physics.add.sprite(400, pipeVerticalPosition, 'pipe').setOrigin(0, 1);
-  lowerPipe = this.physics.add.sprite(400, upperPipe.y + pipeVerticalDistance, 'pipe').setOrigin(0, 0);
+  for (let index = 0; index < PIPES_TO_RENDER; index++) {
+    pipeHorizontalDistance += 400;
+    const pipeVerticalDistance = Phaser.Math.Between(PIPE_DISTANCE_RANGE[0], PIPE_DISTANCE_RANGE[1]);
+    const pipeVerticalPosition = Phaser.Math.Between(0 + 20, (gameConfig.height as number) - 20 - pipeVerticalDistance);
+
+    upperPipe = this.physics.add.sprite(pipeHorizontalDistance, pipeVerticalPosition, 'pipe').setOrigin(0, 1);
+    lowerPipe = this.physics.add
+      .sprite(pipeHorizontalDistance, upperPipe.y + pipeVerticalDistance, 'pipe')
+      .setOrigin(0, 0);
+
+    upperPipe.body.velocity.x = -150;
+    lowerPipe.body.velocity.x = -150;
+  }
 
   bird = this.physics.add.sprite(initialPos.x, initialPos.y, 'bird').setOrigin(0);
-  bird.body.velocity.x = VELOCITY;
   bird.body.gravity.y = 400;
 
   this.input.keyboard.on('keyup-SPACE', flap);
